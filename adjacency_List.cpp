2416aa0f95548be2080fe8e7a7e99efc815654dd
paritosh9 <paritoshajgaonkar@gmail.com>
2020-01-04 15:06:36 -0800

    Create adjacency_List.cpp

---
+++ adjacency_List.cpp
@@ -0,0 +1,45 @@
+*******************************************************************************/
+
+#include <iostream>
+#include <list>
+#include <iterator>
+using namespace std;
+
+class Graph {
+    private :
+        int numVertices;
+        list <int> *adjList;
+    
+    public :
+        // Graph Class Constructor 
+        Graph(int vertex_count) {
+            adjList = new list<int>[vertex_count];
+        }
+        
+        // Method to addWeightEdge, currently only adds Edge, to be updated to add edge and weight
+        void addWeightEdge(int src, int dest) {
+            adjList[src].push_back(dest);
+        }
+        
+        //Method to remove Edge
+        void removeWeighEdge(int src, int dest) {
+            adjList[src].remove(dest);
+        }
+        
+        void isEdge(int src, int dest) {
+            auto it;
+            for(it = adjList[src].begin(); it < adjList[src].end(); it++) {
+                if (*it == dest) {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+};
+
+int main()
+{
+    Graph(4);
+
+    return 0;
+}
