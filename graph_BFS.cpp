5092090a2574a2a3aa5c4cf14644bf41ed413bcd
paritosh9 <paritoshajgaonkar@gmail.com>
2020-01-05 00:06:44 -0800

    Update graph_BFS.cpp

--- graph_BFS.cpp
+++ graph_BFS.cpp
@@ -15,23 +15,29 @@ void Graph::BFS(int startVertex) {
     list <node> queue;
     
     visited_vertices[startVertex] = 1;
-    
     int current_vertex = startVertex;
-    queue.push_back(*adjList[current_vertex].begin());
+    
+    node startNode;
+    startNode.dest = current_vertex;
+    startNode.weight = 0;
+    queue.push_back(startNode);
     
     while(!queue.empty()) {
+        current_vertex = queue.front().dest;
+        //cout <<current_vertex <<endl;
         queue.pop_front();
-        cout << current_vertex << "-> ";
+        
+        cout << " -> " << current_vertex ;
         for(auto it = adjList[current_vertex].begin(); it != adjList[current_vertex].end(); it++) {
             node iterator_node = *it;
-            if(visited_vertices[iterator_node.dest] == 0) {
+            if(not visited_vertices[iterator_node.dest]) {
+                visited_vertices[iterator_node.dest] = 1;
                 queue.push_back(*it);
             }
         }
-        current_vertex = queue.front().dest;
-        queue.pop_front();
-        visited_vertices[current_vertex] = 1;
-    }
+        
+            
+    };
     
     for(int cnt = 0 ; cnt < numVertices; cnt++){
         cout << "\n" << "visited_vertices[" << cnt << "] : " << visited_vertices[cnt];
@@ -42,12 +48,15 @@ void Graph::BFS(int startVertex) {
 
 int main()
 {
-    Graph g(4);
+    Graph g(8);
     g.addWeightEdge(0,2,1);
     g.addWeightEdge(0,1,9);
     g.addWeightEdge(1,3,2);
     g.addWeightEdge(2,1,3);
     g.addWeightEdge(3,0,4);
+    g.addWeightEdge(4,3,22);
+    g.addWeightEdge(7,3,45);
+    g.addWeightEdge(5,2,55);
     
     g.print_adjList();
     
