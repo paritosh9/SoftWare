bf38dc9c4a01485fb310083721bc316604f9a074
paritosh9 <paritoshajgaonkar@gmail.com>
2020-10-16 11:31:04 -0700

    Update experiment.cpp
    multidimensional STL vector

--- c++/c++_experiments/experiment.cpp
+++ c++/c++_experiments/experiment.cpp
@@ -1,1 +1,19 @@
-#
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+
+	vector< vector<int> > grid(4, {1, 2, 3, 4});
+	grid[2].push_back(34);
+	for (vector< vector<int> >::iterator row = grid.begin(); row != grid.end(); row++) {
+		for (vector<int>::iterator col = (*row).begin(); col != (*row).end(); col++) {
+			printf("%d ", (*col));
+		}
+		printf("\n");
+	}
+
+	system("pause");
+	return 0;
+}
